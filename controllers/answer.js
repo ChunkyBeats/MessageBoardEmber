ba4ef296be6edbe4781afdf78536d7e495e8758b
Cory Ribson <crbi2550@gmail.com>
2015-06-12 14:00:03 -0700

    Fixes minor error with deletion of answer (threw error but did not break page)

--- controllers/answer.js
+++ controllers/answer.js
@@ -1,10 +1,13 @@
 Messageboard.AnswerController = Ember.Controller.extend({
+  needs: ['question'],
   actions: {
     deleteanswer: function() {
       if(confirm('Are You Sure?')){
+        var question = this.get('controllers.question.model');
         this.get('model').destroyRecord();
-        this.transitionToRoute('question');
+        question.save();
+        this.transitionToRoute('questions');
       }
     }
   }
-})
+});
