6d6cb1d674e2a9b71858110176ff0077f3d93a1c
Cory Ribson <crbi2550@gmail.com>
2015-06-12 11:20:32 -0700

    Add ability to add answers to questions

--- models/answer.js
+++ models/answer.js
@@ -2,5 +2,5 @@ Messageboard.Answer = DS.Model.extend({
   answer: DS.attr(),
   author: DS.attr(),
 
-  answer: DS.belongsTo('question', {async: true})
+  question: DS.belongsTo('question', {async: true})
 });
--- router.js
+++ router.js
@@ -1,5 +1,7 @@
 Messageboard.Router.map(function(){
   this.resource('questions', {path: '/'});
   this.resource('new-question');
-  this.resource('question', {path: 'questions/:question_id'});
+  this.resource('question', {path: 'questions/:question_id'}, function() {
+    this.resource('new-answer');
+  });
 });
